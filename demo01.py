80d1325f28ab380c9824d00f4735557c24733706
zx <1139390512@qq.com>
2022-02-17 17:19:08 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,13 @@
+""" print("hello world!")
+print(123)
+print([1])
+print("哈哈"+"嘻嘻")
+print("哈哈"*50)
+
+a = input("请输入:")
+b = input("请输入:")
+print("input输入: ",a+b)
+"""
+a = input("请输入:")
+b = input("请输入:")
+print("运算的结果是:",len(a)+len(b))
